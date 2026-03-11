0cd46a8c646843c7b33d32f3e2a6cda0c3933e9a
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2026-03-12 02:50:03 +0700

    Membuat tampilan dengan index.html dan penyesuaian pada KtpController

--- src/main/java/com/example/demo/controller/KtpController.java
+++ src/main/java/com/example/demo/controller/KtpController.java
@@ -8,10 +8,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 import java.util.Map;
 
+
+@CrossOrigin(origins = "*")
 @RestController
 public class KtpController {
 
