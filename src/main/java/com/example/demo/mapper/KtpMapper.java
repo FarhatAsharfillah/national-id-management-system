81846f8d97a5b766fa6ab0a6f66ba8671d0ff733
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2026-03-12 01:13:29 +0700

    Membuat interface KtpMapper.java di dalam package mapper. Ini akan otomatis mengubah Ktp (Entity) menjadi KtpDto

--- src/main/java/com/example/demo/mapper/KtpMapper.java
+++ src/main/java/com/example/demo/mapper/KtpMapper.java
@@ -1,4 +1,13 @@
 package com.example.demo.mapper;
 
-public class KtpMapper {
+import com.example.demo.model.dto.KtpDto;
+import com.example.demo.model.entity.Ktp;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface KtpMapper {
+    KtpMapper MAPPER = Mappers.getMapper(KtpMapper.class);
+
+    KtpDto toKtpDtoData(Ktp ktp);
 }
